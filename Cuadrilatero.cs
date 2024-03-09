c01d44759c0f6cd9215baee79c334acc9de7abfd
MENDO <MENDO@MENDOZALZ>
2024-03-09 16:58:30 -0500

    Agregar archivos de proyecto.

---
+++ Cuadrilatero.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular_el_area_y_el_perimetro_POO
+{
+    internal class Cuadrilatero
+    {
+        private double lado1;
+        private double lado2;
+
+        public Cuadrilatero(double lado) // Para calcular el parametro solo basta con recibir un solo parametro como valor
+        {
+            this.lado1 = lado;
+            this.lado2 = lado;
+        }
+
+        public Cuadrilatero(double _lado1, double _lado2) // Para clcular el area o perimetro de un rectangulo se necesitan dos parametros como valor
+        {
+            this.lado1 += _lado1;
+            this.lado2 += _lado2;
+        }
+
+        public double perimetro()
+        {
+            return (lado1 + lado2) * 2;
+        }
+
+        public double area()
+        {
+            return lado1 * lado2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcular_el_area_y_el_perimetro_POO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuadrilatero c;
+            Console.WriteLine("Selecciona una opción valida para calcular el área y perimetro de: \n Un Cuadrado en la opción 1 \n Un Rectangulo en la opción 2");
+            double opcion = double.Parse(Console.ReadLine());
+
+            if (opcion == 1)
+            {
+                Console.WriteLine("Has seleccionado calcular el área y perimetro de un Cuadrado, ingresa tu valor a calcular");
+                double valor = double.Parse(Console.ReadLine());
+                c = new Cuadrilatero(valor);
+                Console.WriteLine("El y perimetro de tu Cuadrado es " + c.perimetro() + " y el área de tu Cuadrado es " + c.area());
+            }
+            else if (opcion == 2)
+            {
+                Console.WriteLine("Has seleccionado calcular el área y perimetro de un Rectangulo, ingresa el primer valor a calcular");
+                double valor1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Ingresa el segundo valor a calcular");
+                double valor2 = double.Parse(Console.ReadLine());
+                c = new Cuadrilatero(valor1, valor2);
+                Console.WriteLine("El y perimetro de tu Cuadrado es " + c.perimetro() + " y el área de tu Cuadrado es " + c.area());
+            }
+            else
+            {
+                Console.WriteLine("Solo son validas las opciones 1 y 2");
+                c = new Cuadrilatero(0);
+            }
+            Console.Read();
+        }
+    }
+}
